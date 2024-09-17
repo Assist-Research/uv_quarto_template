6100bb93776b2f819d6b3ead38a134d66a19494a
Dave Hastie <dh@assistresearch.io>
2024-09-17 20:52:32 +0100

    Initial commit

--- template/tasks.py
+++ template/tasks.py
@@ -4,3 +4,16 @@ from invoke import task
 @task
 def check(c):
     c.run("pre-commit run --all-files")
+
+
+@task
+def docs(c):
+    with c.cd("docs"):
+        c.run("quartodoc build")
+        c.run("quarto render")
+    
+
+@task
+def publish(c):
+    with c.cd("docs"):
+        c.run("quarto publish netlify")
